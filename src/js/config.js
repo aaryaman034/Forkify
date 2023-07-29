373f399160a44e480d7b823ffed426daa1993510
Aaryaman Prakash <113224774+aaryaman034@users.noreply.github.com>
2023-07-29 12:21:14 +0530

    Add files via upload

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'fc65c67f-58f0-4bc7-ae5f-4d1ed73a5e0a';
+export const MODAL_CLOSE_SEC = 2.5;
